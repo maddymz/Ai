74e540b77744d1d544d0291492e6e4b049186e42
madhukar <madhukar4u.rm@gmail.com>
2018-10-01 21:05:00 -0700

    insertion sort correct code

--- slection_sort.py
+++ slection_sort.py
@@ -1,21 +1,34 @@
 #selection sort algorithm using python 
+"""
+selection sort steps:
+ - find the smallest element in the array
+ - exchange it with the intial position 
+ - find second smallest and exchange it with second  
+"""
 import random
 
+#function to find teh smallest element of the array
+def element_smallest(value, length, array, key):
+    j = value -1
+    for i in range(value, length):
+           if(array[j] > array[i]):
+                array[j] = array[i]
+                array[i] = key 
+                key = array[j]
+    return key
+           
+      
 #function to sort the array
 def selection_sort(array):
     for j in range(len(array)):
         key = array[j]
-        i= j+1
-        while i < len(array) and array[i] < key:
-                array[i-1] = array[i]
-                array[i] = key
-                
+        smallest_element = element_smallest(j+1, len(array), array, key)            # slection of the smallest element 
+        array[j] = smallest_element                                                 # exchange the smallest element with a[j]
 
-    return array
+    return array       
 
 # intializatoin and fucntion call 
-
-unsorted_array_list = random.sample(range(50), 5)
+unsorted_array_list = random.sample(range(50), 20)
 sorted_array_list = selection_sort(unsorted_array_list)
 print("Sorted array is :", sorted_array_list)
 
