7468d62c4bb3e2d157d9b7cd58bcd2ca8c2af796
madhukar <madhukar4u.rm@gmail.com>
2018-10-12 12:47:21 -0700

    more optimized code , with two variables

--- max_sub_array_brute_force.py
+++ max_sub_array_brute_force.py
@@ -1,22 +1,25 @@
 #Brute force approach to solve a max subarray problem 
-
+import time
+from sysconfig import sys
 #function to find max sub-array
+
 def max_subarray (array):
-    max_sum = 0
+    max_sum = -sys.maxsize -1
+    print(-sys.maxsize-1 )
     for i in range(len(array)):
         sum_so_far = 0                                                  #variable to keep track of teh total sum
         for j in range( i, len(array)):
-           current_sum = sum_so_far + array[j]                          #current sum calculation 
-           if (sum_so_far < current_sum and max_sum < current_sum):               
-               max_sum = current_sum                                    #storing max sum
+           sum_so_far = sum_so_far + array[j]                          #current sum calculation 
+           if (max_sum < sum_so_far):               
+               max_sum = sum_so_far                                    #storing max sum
                index_low = i                                            #storing lower index of max subarray
                index_high = j                                           #storing higher index of max subarray
-           sum_so_far = current_sum                                    
-
+                                             
     return index_low, index_high, max_sum
 
 #variable intialization and function call
-array = [1,3,-5,-9,4,9]
+array = [1,3,-5,-9,4,9,3,5,7,0,-7,-4,-6]
+
 low_index,high_index,subarray_max_sum = max_subarray(array)
 print("Max subarray is array[{},{}] with sum {}".format(low_index, high_index, subarray_max_sum))
 
