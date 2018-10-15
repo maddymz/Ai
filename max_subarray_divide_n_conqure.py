39a37ba355153647cd6f42f8b5fde765990f3981
madhukar <madhukar4u.rm@gmail.com>
2018-10-15 04:25:04 -0700

    algoritm to multiply two binary numbers restoring 2's complement divsion

--- max_subarray_divide_n_conqure.py
+++ max_subarray_divide_n_conqure.py
@@ -4,6 +4,15 @@ from sysconfig import sys
 from numpy import asarray
 import timeit as timeit
 
+
+#function to calculate array of energy difference
+def find_array_energy_difference(array):
+    array_diff = []
+    for i in range(1, len(array)):
+        energy_difference = array[i] - array[i-1]
+        array_diff.append(energy_difference)
+    return asarray(array_diff)
+
 #function to find the max_cross_subarray
 def find_max_crossing_subarray(array, low, mid, high):
     left_sum = -sys.maxsize -1                                        #intialize left_sum as maximum negative number
@@ -22,7 +31,7 @@ def find_max_crossing_subarray(array, low, mid, high):
             right_sum = sum_so_far
             max_right = j
             
-    return max_left, max_right, left_sum + right_sum
+    return max_left, max_right+1, left_sum + right_sum
     
 #function to find the maximum subarray
 def find_max_subarray(array, low, high):
@@ -34,6 +43,9 @@ def find_max_subarray(array, low, high):
         right_low, right_high, right_sum = find_max_subarray(array, mid+1, high)
         cros_low, cross_high, cross_sum = find_max_crossing_subarray(array, low, mid, high)
 
+        print(left_low,left_high)
+        print(right_low, right_high)
+        print(cros_low, cross_high)
         if(left_sum > right_sum and left_sum > cross_sum):
             return left_low, left_high, left_sum
         elif (right_sum > left_sum and right_sum > cross_sum):
@@ -42,11 +54,12 @@ def find_max_subarray(array, low, high):
             return cros_low, cross_high, cross_sum
 
 # variable intialization and function call
-array_list = [1,3,-5,-9,4,9,3,5,7,0,-7,-4,-6]
+array_list =[100,113,110,85,105,102,86,63,81,101,94,106,101,79,94,90,97]
+array_diff = find_array_energy_difference(array_list)
 array = asarray(array_list)
-array_size = len(array)
+array_size = len(array_diff)
 Start = timeit.default_timer()
-low, high, max_sum = find_max_subarray(array, 0, array_size-1)
+low, high, max_sum = find_max_subarray(array_diff, 0, array_size-1)
 End = timeit.default_timer()
 print("The maximum subarray is subarray[{},{}] with sum {}".format(low,high,max_sum))
 print("run time: ", End - Start)
